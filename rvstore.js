4a1b457646d43f6f1a741c2d333cff95b7b62b93
WSiaB <johnl@kirkersoft.com>
2016-05-23 17:58:52 -0700

    First commit

---
+++ rvstore.js
@@ -0,0 +1,49 @@
+function randImg(){
+	var myImages = Array();
+	myImages[0] = "rvheaderpic-00.jpg";
+	myImages[1] = "rvheaderpic-01.jpg"; 
+	myImages[2] = "rvheaderpic-02.jpg"; 
+	myImages[3] = "rvheaderpic-03.jpg"; 
+	myImages[4] = "rvheaderpic-04.jpg"; 
+	myImages[5] = "rvheaderpic-05.jpg"; 
+	myImages[6] = "rvheaderpic-06.jpg"; 
+	myImages[7] = "rvheaderpic-07.jpg"; 
+	myImages[8] = "rvheaderpic-08.jpg"; 
+	myImages[9] = "rvheaderpic-09.jpg"; 
+	myImages[10] = "rvheaderpic-10.jpg"; 
+	myImages[11] = "rvheaderpic-11.jpg"; 
+	myImages[12] = "rvheaderpic-12.jpg"; 
+	myImages[13] = "rvheaderpic-13.jpg"; 
+	myImages[14] = "rvheaderpic-14.jpg";
+	myImages[15] = "rvheaderpic-15.jpg";
+	myImages[16] = "rvheaderpic-16.jpg";
+	myImages[17] = "rvheaderpic-17.jpg";
+	myImages[18] = "rvheaderpic-18.jpg";
+	myImages[19] = "rvheaderpic-19.jpg";
+	myImages[20] = "rvheaderpic-20.jpg";
+	myImages[21] = "rvheaderpic-21.jpg";
+	myImages[22] = "rvheaderpic-22.jpg";
+	myImages[23] = "rvheaderpic-23.jpg";
+	myImages[24] = "rvheaderpic-24.jpg";
+	myImages[25] = "rvheaderpic-25.jpg";
+	myImages[26] = "rvheaderpic-26.jpg";
+	myImages[27] = "rvheaderpic-27.jpg";
+	myImages[28] = "rvheaderpic-28.jpg";
+	var image = "images/" + myImages[Math.floor(Math.random()*myImages.length)]
+	return image;
+}
+
+function randImg2(){
+var myImages2 = Array();
+myImages2[0] = "gorving-00.jpg";
+myImages2[1] = "gorving-01.jpg";
+myImages2[2] = "gorving-02.jpg";
+myImages2[3] = "gorving-03.jpg";
+myImages2[4] = "gorving-04.jpg";
+myImages2[5] = "gorving-05.jpg";
+myImages2[6] = "gorving-06.jpg";
+myImages2[7] = "gorving-07.jpg";
+myImages2[8] = "gorving-08.jpg";
+var image = "images/" + myImages2[Math.floor(Math.random()*myImages2.length)]
+return image;
+}
